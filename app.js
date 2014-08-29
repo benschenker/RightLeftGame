c2420a1e53ff56618c69b3b8d63e5b6e81013951
benschenker <binsch@gmail.com>
2014-08-29 18:36:13 -0400

    Changed to flashing colors instead of checkmarks

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
-(function() {
+var app = angular.module('gameLvR', [])
+.controller('GameController', ['$scope','$timeout',function($scope,$timeout){
+	$scope.commands = ["LEFT", "RIGHT"];
+	$scope.command = "";
+	$scope.points = 0;
+	$scope.clickLock = false; //will respond to clicks only if false
+	$scope.correctnessClass = "";
 	
-	var app = angular.module('gameLvR', ['gameFilters'])
-	.controller('GameController', ['$scope','$timeout',function($scope,$timeout){
-		$scope.commands = ["LEFT", "RIGHT"];
-		$scope.command = "";
-		$scope.feedback = -1;
-		$scope.points = 0;
-		$scope.tryClick = function(text){
+	$scope.setCorrectness = function(text){
+		$scope.correctnessClass = text;
+	}
+	
+	$scope.tryClick = function(text){
+		if(!$scope.clickLock){
+			$scope.clickLock = true;
 			if($scope.command == text){
 				$scope.points++;
-				$scope.feedback = 1;
+				$scope.setCorrectness("correct");
 				$scope.reset();
 			}
 			else{
-				$scope.points--;
-				$scope.feedback = 0;
+				$scope.points-=5;
+				$scope.setCorrectness("wrong");
 				$scope.reset();
 			}
 		}
-		$scope.reset = function(){
-				$timeout($scope.resetCommand,500);
-			
-		}
-		$scope.resetCommand = function(){
-			$scope.command = $scope.commands[Math.random()>.5 ? 1:0];
-			$scope.feedback = -1;
-		}
-		$scope.resetCommand(); //initialize our command
-	}]);
-
-})();
+	}
+	
+	$scope.reset = function(){
+			$timeout($scope.resetCommand,100);
+	}
+	
+	$scope.resetCommand = function(){
+		$scope.command = $scope.commands[Math.random()>.5 ? 1:0];
+		$scope.setCorrectness("l");
+		$scope.clickLock = false;
+	}
+	
+	$scope.resetCommand(); //initialize our command
+	
+	
+}]);
 
 
